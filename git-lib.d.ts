3aeeb89d7da3f51601936d1777eb12252246f3d4
yugamg <yugam.ghogia@tieto.com>
2019-10-04 14:11:21 +0530

    new commit

--- git-lib.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public-api';
-export { TsuButtonComponent as ɵa } from './lib/Button/tsu-button/tsu-button.component';
--- public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/foo-lib.service';
-export * from './lib/foo-lib.component';
-export * from './lib/foo-lib.module';
-export * from './lib/stylebundle';
